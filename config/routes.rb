31f71cc44c6c3f15b938202dddcea52e3dc94e13
Kim Nguyen <nnhk23@gmail.com>
2021-01-16 01:49:35 -0800

    added get recipe and get recipe details for search and recipe display functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post "/login", to: "users#login"
   get "/getuser", to: "users#get_user"
+  get "/getrecipe", to: 'spoonacular_api#show'
+  get "/getrecipedetails", to: 'spoonacular_api#get_recipe'
 
 end
